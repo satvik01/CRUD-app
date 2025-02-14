81c853783c9e69c9d47cc623c786d26f963e2a9c
satvik01 <57486408+satvik01@users.noreply.github.com>
2025-02-14 15:15:12 +0530

    Update connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
     try{
         // mongodb connection string
         const con = await mongoose.connect(process.env.MONGO_URI, {
+            dbName: process.env.crud_app,
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
